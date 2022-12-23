40a8823fafe1b839938013e87ecb8c0b20403249
Justin Dickson <112723736+jay-nakagawa@users.noreply.github.com>
2022-12-22 23:25:38 -0500

    started 1.5- header working

--- Part 1/Exercises/courseinfo/src/App.js
+++ Part 1/Exercises/courseinfo/src/App.js
@@ -6,39 +6,40 @@ const Header = (props) => {
   )
 }
 
-const Content = (props) => {
-  return (
-    <div>
-      <Part part={props.parts[0].name} exercise={props.parts[0].exercises} />
-      <Part part={props.parts[1].name} exercise={props.parts[1].exercises} />
-      <Part part={props.parts[2].name} exercise={props.parts[2].exercises} />
-    </div>
-  )
-}
+// const Content = (props) => {
+//   return (
+//     <div>
+//       <Part part={props.parts[0].name} exercise={props.parts[0].exercises} />
+//       <Part part={props.parts[1].name} exercise={props.parts[1].exercises} />
+//       <Part part={props.parts[2].name} exercise={props.parts[2].exercises} />
+//     </div>
+//   )
+// }
 
-const Part = (props) => {
-  return (
-    <div>
-      {props.part} {props.exercise}
-      
-    </div>
-  )
-}
+// const Part = (props) => {
+//   return (
+//     <div>
+//       {props.part} {props.exercise}
 
-
+//     </div>
+//   )
+// }
 
 
-const Footer = (props) => {
-  return (
-    <div>
-      <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
-    </div>
-  )
-}
+
+
+// const Footer = (props) => {
+//   return (
+//     <div>
+//       <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
+//     </div>
+//   )
+// }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
+  const course = {
+  name: 'Half Stack application development',
+  parts: [
     {
       name: 'Fundamentals of React',
       exercises: 10
@@ -52,17 +53,16 @@ const App = () => {
       exercises: 14
     }
   ]
-
+}
 
   return (
     <div>
 
-      {console.log(parts)}
-      <Header course={course} />
+      {/* {console.log(parts)} */}
+      <Header course={course.name} />
+      {/* <Content parts={parts} />
+      <Footer parts={parts} /> */}
 
-
-      <Content parts={parts}/>
-      <Footer parts={parts} />
     </div>
   )
 }
