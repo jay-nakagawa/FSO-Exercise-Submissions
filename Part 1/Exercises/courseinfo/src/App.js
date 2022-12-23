1b45e60313ef2ec69cb0d76bc48072aa6e28c3aa
Justin Dickson <112723736+jay-nakagawa@users.noreply.github.com>
2022-12-22 20:07:34 -0500

    completed 1.3

--- Part 1/Exercises/courseinfo/src/App.js
+++ Part 1/Exercises/courseinfo/src/App.js
@@ -9,9 +9,9 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <Part part={props.parts[0]} exercise={props.exercises[0]} />
-      <Part part={props.parts[1]} exercise={props.exercises[1]} />
-      <Part part={props.parts[2]} exercise={props.exercises[2]} />
+      <Part part={props.part1.name} exercise={props.part1.exercises} />
+      <Part part={props.part2.name} exercise={props.part2.exercises} />
+      <Part part={props.part3.name} exercise={props.part3.exercises} />
     </div>
   )
 }
@@ -20,6 +20,7 @@ const Part = (props) => {
   return (
     <div>
       {props.part} {props.exercise}
+      
     </div>
   )
 }
@@ -30,31 +31,39 @@ const Part = (props) => {
 const Footer = (props) => {
   return (
     <div>
-      <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+      <p>Number of exercises {props.part1.exercises + props.part2.exercises + props.part3.exercises}</p>
     </div>
   )
 }
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
-  const parts = ['Fundamentals of React','Using props to pass data','State of a component' ]
-  const exercises = [10,7,14]
+  const parts = ['Fundamentals of React', 'Using props to pass data', 'State of a component']
+  const exercises = [10, 7, 14]
 
   return (
     <div>
-          
-      <Header course={course} />          
-      <Content parts={parts} exercises={exercises}  />           
-      <Footer exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
-                
-    
+
+      {console.log(part1)}
+      <Header course={course} />
+
+
+      <Content part1={part1} part2={part2} part3={part3}/>
+      <Footer part1={part1} part2={part2} part3={part3} />
     </div>
   )
 }
