9a211d1ed31f2cc26ddb36943451edb2fafec757
jay-nakagawa <j.dickson2203@gmail.com>
2022-12-27 01:26:08 -0500

    2.3 completed

--- Part 2/Exercises/courseinfo/src/App.js
+++ Part 2/Exercises/courseinfo/src/App.js
@@ -68,15 +68,24 @@ const Course = ({course}) => {
   console.log("this is",course.name);
   
   return(
-    <div>
+    <div>    
       
-      
-       <Header name={course.name} />
+      <Header name={course.name} />
       <Content parts={course.parts} />
-      {/* <Total parts={course.parts} /> */}
+      <Total parts={course.parts} />
     </div>
   
   )
+    }
+
+    const Total = ({parts}) => {
+
+      const total = parts.reduce((runningTotal,num)=> runningTotal+num.exercises,0)
+      return (
+        <div>
+          <p>Number of exercises {total}</p>
+        </div>
+      )
     }
   
   const App = () => {
