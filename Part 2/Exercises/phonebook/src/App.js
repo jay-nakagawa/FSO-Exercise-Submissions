daaead73c0c78b5be3e316aadee3f7aaa8eb374c
Justin Dickson <112723736+jay-nakagawa@users.noreply.github.com>
2022-12-30 14:28:31 -0500

    2.8 -added second input for number

--- Part 2/Exercises/phonebook/src/App.js
+++ Part 2/Exercises/phonebook/src/App.js
@@ -11,8 +11,13 @@ const App = () => {
     { name: 'Arto Hellas' }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+  const handleNumberChange = (event) => {
     console.log(event.target.value)
     setNewName(event.target.value)
   }
@@ -39,14 +44,19 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      
       <form onSubmit={addName}>
         <div>
           name: <input value={newName} onChange={handleNameChange}/>
         </div>
+        
+        <div>number: <input value={newNumber} onChange={handleNumberChange} /></div>
+        
         <div>
           <button type="submit">add</button>
         </div>
       </form>
+      
       <h2>Numbers</h2>
       <div>
       <div>debug: {newName}</div>
