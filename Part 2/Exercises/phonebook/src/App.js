3c5dd578d0a93b75b4394495228c63a282d5aac8
Justin Dickson <112723736+jay-nakagawa@users.noreply.github.com>
2022-12-31 12:19:55 -0500

    2.9 - started -added hardcoded user data

--- Part 2/Exercises/phonebook/src/App.js
+++ Part 2/Exercises/phonebook/src/App.js
@@ -8,7 +8,10 @@ const Person = ({ person }) => {
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
