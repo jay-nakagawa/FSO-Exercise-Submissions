a422692f8d7179aa42853adbff9c6ba9db674e8d
Justin Dickson <112723736+jay-nakagawa@users.noreply.github.com>
2023-01-01 16:29:23 -0500

    2.11 completed - add axios and use effect to get inital state from the db

--- Part 2/Exercises/phonebook/src/App.js
+++ Part 2/Exercises/phonebook/src/App.js
@@ -1,20 +1,31 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Person from "./components/person"
 import Filter from "./components/filter"
 import Form from "./components/form"
+import axios from 'axios'
 
 
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+    // { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    // { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    // { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    // { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
-  const [newFilter, setNewFilter] = useState('A')
+  const [newFilter, setNewFilter] = useState('')
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
 
   const handleNameChange = (event) => {
     console.log(event.target.value)
@@ -47,7 +58,7 @@ console.log(personObject)
       setNewNumber('')
     }
    
-    
+
   }
   
 
