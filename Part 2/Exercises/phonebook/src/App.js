a7626f8d6542aebf0e81bf7742a5ff6ffbc74623
Justin Dickson <112723736+jay-nakagawa@users.noreply.github.com>
2023-01-04 15:41:04 -0500

    .

--- Part 2/Exercises/phonebook/src/App.js
+++ Part 2/Exercises/phonebook/src/App.js
@@ -70,7 +70,7 @@ const App = () => {
     console.log(nameToChange)
     console.log(personObject);
     if (nameToChange) {
-     if(window.confirm('test')){
+     if(window.confirm(`${nameToChange.name} is already in the phonebook, replace old number with a new one?`)){
       personService
       .update(nameToChange.id,personObject)
       .then((response)=>{
