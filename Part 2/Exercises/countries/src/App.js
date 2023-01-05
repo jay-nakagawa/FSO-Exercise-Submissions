87389b69b808d173ac4eb62c07fab6a3a46da230
Justin Dickson <112723736+jay-nakagawa@users.noreply.github.com>
2023-01-05 14:52:46 -0500

    -added axios -app can make get req and render data

--- Part 2/Exercises/countries/src/App.js
+++ Part 2/Exercises/countries/src/App.js
@@ -1,9 +1,32 @@
+//useEffect allows for use of the effect hook. this is used for fetching data
+import { useState,useEffect } from "react"; 
+//axios allow communication between front and backend
+import axios from 'axios'
 
 
 function App() {
+  //state for all countries
+  const [countries, setCountries] = useState([])
+
+  useEffect(() => {
+    //req's server to send name,capital,currencies from all countries. server responds and data is set in countries.
+    axios
+    .get("https://restcountries.com/v2/all?fields=name,capital,currencies")
+    .then((response) => {
+      console.log(response.data)
+      setCountries(response.data);
+    });
+  }, []); // effect takes 2 param. 2nd param defines when effect is used, effect by default run after every render. empty array means it is only ran on the first render.
+
+
   return (
+
     <div className="App">
-      est
+      <ul>yes
+        <li>{countries[0].name}</li>
+        <li>{countries[1].name}</li>
+        <li>{countries[2].name}</li>
+      </ul>
     </div>
   );
 }
