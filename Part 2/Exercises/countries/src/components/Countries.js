9bf7393abf0ac481424280e43a92b204b62a84e0
Justin Dickson <112723736+jay-nakagawa@users.noreply.github.com>
2023-01-06 14:56:05 -0500

    more refactoring

--- Part 2/Exercises/countries/src/components/Countries.js
+++ Part 2/Exercises/countries/src/components/Countries.js
@@ -1,39 +1,47 @@
+const CountryData = ({ countriesToShow }) => {
+  let languages = countriesToShow[0].languages.map(
+    (languages) => languages.name
+  );
+
+  return (
+    <div>
+      {` ${countriesToShow[0].name}'s capital is 
+            ${countriesToShow[0].capital} and they speak: `}
+      <ul>
+        {languages.map((language) => (
+          <li key={language}>{language}</li>
+        ))}
+      </ul>
+      <img src={countriesToShow[0].flags.png} alt="flag" />
+    </div>
+  );
+};
+
 const Countries = ({ countriesToShow }) => {
-   
+  const countryCount = countriesToShow.length;
+  console.log(countryCount);
 
-    if (countriesToShow.length === 0) {
-      return <div>Search for a country to begin</div>;
-    } else if (countriesToShow.length === 1) {
-      console.log(countriesToShow[0].languages);
+  if (countryCount === 0) {
+    return <div>Search for a country to begin</div>;
+  } else if (countryCount === 1) {
+    console.log(countriesToShow[0].languages);
 
-      let languages = countriesToShow[0].languages.map(
-        (languages) => languages.name
-      );
-      
+    return (
+      <div>
+        <CountryData countriesToShow={countriesToShow} />
+      </div>
+    );
+  } else if (countryCount <= 10) {
+    return (
+      <div>
+        {countriesToShow.map((country) => (
+          <div key={country.name}>{country.name}</div>
+        ))}
+      </div>
+    );
+  } else if (countryCount >= 10) {
+    return <div>Too many matches, please specify another filter.</div>;
+  }
+};
 
-      return (
-        <div>
-          {` ${countriesToShow[0].name}'s capital is 
-          ${countriesToShow[0].capital} and they speak: `}
-          <ul>
-            {languages.map((language) => (
-              <li key={language}>{language}</li>
-            ))}
-          </ul>
-          <img src={countriesToShow[0].flags.png} alt="flag" />
-        </div>
-      );
-    } else if (countriesToShow.length <= 10) {
-      return (
-        <div>
-          {countriesToShow.map((country) => (
-            <div key={country.name}>{country.name}</div>
-          ))}
-        </div>
-      );
-    } else if (countriesToShow.length >= 10) {
-      return <div>Too many matches, please specify another filter.</div>;
-    }
-  };
-
-  export default Countries
+export default Countries;
