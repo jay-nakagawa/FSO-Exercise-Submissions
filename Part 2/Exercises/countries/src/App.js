93c812a0dc55bac2169c8c37d25fb8b76bad2f84
Justin Dickson <112723736+jay-nakagawa@users.noreply.github.com>
2023-01-05 19:22:21 -0500

    2.12 complete - user can see country flag

--- Part 2/Exercises/countries/src/App.js
+++ Part 2/Exercises/countries/src/App.js
@@ -1,78 +1,65 @@
 //useEffect allows for use of the effect hook. this is used for fetching data
-import { useState,useEffect } from "react"; 
+import { useState, useEffect } from "react";
 //axios allow communication between front and backend
-import axios from 'axios'
-
+import axios from "axios";
 
 function App() {
-  
-  const [countries, setCountries] = useState([]) //holds all the countries
+  const [countries, setCountries] = useState([]); //holds all the countries
   const [filter, setFilter] = useState(""); //holds search input
-  const [countriesToShow, setCountriesToShow] = useState([]) //holds countries that meet search parameters
+  const [countriesToShow, setCountriesToShow] = useState([]); //holds countries that meet search parameters
 
   useEffect(() => {
     //req's server to send name,capital,currencies from all countries. server responds and data is set in countries.
     axios
-    .get("https://restcountries.com/v2/all?fields=name,capital,currencies,languages")
-    .then((response) => {
-      // console.log(response.data)
-      setCountries(response.data);
-    });
+      .get(
+        "https://restcountries.com/v2/all?fields=name,capital,currencies,languages,flags"
+      )
+      .then((response) => {
+        // console.log(response.data)
+        setCountries(response.data);
+      });
   }, []); // effect takes 2 param. 2nd param defines when effect is used, effect by default run after every render. empty array means it is only ran on the first render.
 
   const search = (event) => {
     //runs when form is submitted
-    event.preventDefault() // stops submission of form which would cause page reload
-    console.log('button clicked', event.target)
-  }
-  const handleFilterChange = (event) =>{
+    event.preventDefault(); // stops submission of form which would cause page reload
+    console.log("button clicked", event.target);
+  };
+  const handleFilterChange = (event) => {
     // console.log(event.target)
-    setFilter(event.target.value) //this synchronizes changes made to the input with the components state
-    setCountriesToShow( // shows countries that meet the search parameters
-      countries.filter((country) =>{
-   
-            return country.name.toLowerCase().includes(filter.toLowerCase()
-            )
-         
-         })
-      
-    )
-  }
+    setFilter(event.target.value); //this synchronizes changes made to the input with the components state
+    setCountriesToShow(
+      // shows countries that meet the search parameters
+      countries.filter((country) => {
+        return country.name.toLowerCase().includes(filter.toLowerCase());
+      })
+    );
+  };
 
-  const Countries = ({countriesToShow}) => {
-    console.log(countriesToShow)
+  const Countries = ({ countriesToShow }) => {
+    console.log(countriesToShow);
 
-
-    if(countriesToShow.length === 0) {
+    if (countriesToShow.length === 0) {
+      return <div>Search for a country to begin</div>;
+    } else if (countriesToShow.length === 1) {
+      console.log(countriesToShow[0].languages);
+      let languages = countriesToShow[0].languages.map(
+        (languages) => languages.name
+      );
+      console.log(`they speak ${languages}`);
       return (
-        <div>Search for a country to begin</div>
-        )
-    }else if(countriesToShow.length === 1){
-      console.log(countriesToShow[0].languages)
-      let languages = countriesToShow[0].languages.map(languages=> languages.name)
-      console.log(`they speak ${languages}`)
-      return(
         <div>
-         {` ${countriesToShow[0].name}'s capital is 
+          {` ${countriesToShow[0].name}'s capital is 
           ${countriesToShow[0].capital} and they speak: `}
           <ul>
-            {languages.map((language)=>(
+            {languages.map((language) => (
               <li key={language}>{language}</li>
             ))}
-
           </ul>
-          {/* {countriesToShow[0].languages[0].name} */}
-          {/* <ul>
-            {
-            Object.values(countriesToShow[0].languages).map((language)=>(
-              <li key={language}>{language}</li>
-            
-            ))}
-          </ul> */}
-          
+          <img src={countriesToShow[0].flags.png} alt="flag" />
         </div>
-      )
-    }else if(countriesToShow.length<=10){
+      );
+    } else if (countriesToShow.length <= 10) {
       return (
         <div>
           {countriesToShow.map((country) => (
@@ -80,56 +67,24 @@ function App() {
           ))}
         </div>
       );
-    }else if(countriesToShow.length>=10){
-      return(
-        <div>
-          Too many matches, please specify another filter.
-        </div>
-      )
+    } else if (countriesToShow.length >= 10) {
+      return <div>Too many matches, please specify another filter.</div>;
     }
-
-
-  }
-
-  //   return(
-  //     <>
-  //     {
-  //   countriesToShow.map(country => 
-  // <li key={country.name}>
-  //   {country.name}'s capital is {country.capital} </li>
-  //   )}
-   
-  //     </>
-      
-    
-  //   )
-  
-
-  // {
-  //   countriesToShow.map(country => 
-  // <li key={country.name}>
-  //   {country.name}'s capital is {country.capital} </li>
-  //   )}
-
-
-
+  };
 
   return (
-
     <div className="App">
       <h1>Countries</h1>
       <form onSubmit={search}>
-        <h2>Search       
-          <input value={filter} onChange={handleFilterChange}/>
-
+        <h2>
+          Search
+          <input value={filter} onChange={handleFilterChange} />
         </h2>
       </form>
-      
+
       {/* takes the array of filtered countries and renders an li for each one*/}
       {/* {countriesToShow.map(country => <li>{country.name}'s capital is {country.capital} </li>)} */}
       <Countries countriesToShow={countriesToShow} />
-     
-      
     </div>
   );
 }
