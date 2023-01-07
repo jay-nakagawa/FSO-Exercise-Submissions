63fc1ee52d4d247d998bcc1eb99ff3ecb30979a3
Justin Dickson <112723736+jay-nakagawa@users.noreply.github.com>
2023-01-06 20:57:28 -0500

    2.13 finished

--- Part 2/Exercises/countries/src/components/WeatherInfo.js
+++ Part 2/Exercises/countries/src/components/WeatherInfo.js
@@ -4,10 +4,33 @@ import axios from "axios";
 const WeatherData = ({city}) => {
     const [weather,setWeather] = useState([])
     const api_key = process.env.REACT_APP_WEATHER_KEY
+    console.log(api_key)
 
+    useEffect(() => {
+        console.log('effect')
+        axios
+          .get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`)
+          .then(response => {
+            console.log(response.data)
+            setWeather(response.data)
+            
+          })
+      }, [city,api_key])
 
+      
     return(
-        <h2>Weather in{city}</h2>
+        <>
+
+        {/* originally used weather? but array === truthy */}
+        {weather.main? (
+            <>
+                <h2>Weather in {' '}{city}</h2>
+                <div>The temp is {Math.ceil((weather.main.temp) - 273)} degree celsius </div>
+                <div>wind {weather.wind.speed} m/s </div>
+                <img alt="yuh" src ={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} />
+            </>
+        ) :null }
+        </>
     )
 }
 
