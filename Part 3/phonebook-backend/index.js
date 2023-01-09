4669adc54190372f3de4798f5debf15f1f4fcdc7
Justin Dickson <112723736+jay-nakagawa@users.noreply.github.com>
2023-01-08 22:51:35 -0500

    3.4 complete -user can fetch a single resource with error handling.

--- Part 3/phonebook-backend/index.js
+++ Part 3/phonebook-backend/index.js
@@ -36,6 +36,23 @@ app.get("/info", (request, response) => {
 app.get("/api/persons", (request, response) => {
   response.json(persons);
 });
+app.get("/api/persons/:id",(request, response) =>{
+  const id = +request.params.id
+ 
+  
+  const person = persons.find(person=> {
+    console.log(person.id, typeof person.id, id, typeof id, person.id === id)
+    return person.id === id})
+
+    if(person){
+      response.json(person)
+    }else{
+      response.status(404).end()
+    }
+
+  
+})
+
 
 const PORT = 3001;
 app.listen(PORT, () => {
