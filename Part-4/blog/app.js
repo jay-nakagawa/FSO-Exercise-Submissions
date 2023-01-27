bfbe34ade40c5dc6fd626321471579701cc66366
Justin Dickson <112723736+jay-nakagawa@users.noreply.github.com>
2023-01-27 03:25:55 -0500

    .

---
+++ Part-4/blog/app.js
@@ -0,0 +1,13 @@
+
+const config = require('./utils/config')
+
+const url = config.MONGODB_URI
+mongoose.connect(url)
+  .then(result => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
+
+  module.exports = mongoose.model("Blog", blogSchema)
