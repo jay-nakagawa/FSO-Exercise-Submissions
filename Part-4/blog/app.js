731ea685da8f0914c8d42c2fdf4e8eebf808b8a5
Justin Dickson <112723736+jay-nakagawa@users.noreply.github.com>
2023-01-27 03:34:11 -0500

    fubar

--- Part-4/blog/app.js
+++ Part-4/blog/app.js
@@ -1,7 +1,9 @@
+const app = express()
 
 const config = require('./utils/config')
 
 const url = config.MONGODB_URI
+
 mongoose.connect(url)
   .then(result => {
     console.log('connected to MongoDB')
