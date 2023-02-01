902032a15f164cb61a7d96b2c76846ee0ba4728f
Justin Dickson <112723736+jay-nakagawa@users.noreply.github.com>
2023-02-01 18:58:52 -0500

    4.5 complete

--- Part-4/blog/utils/list_helper.js
+++ Part-4/blog/utils/list_helper.js
@@ -8,7 +8,28 @@ const totalLikes = (blogs) => {
     : blogs.reduce((sum, blog) => sum + blog.likes, 0);
 };
 
+const mostLikes = (blogs) => {
+
+
+  let maxLiked = blogs.map(blogs => blogs.likes)
+  let index = maxLiked.indexOf(Math.max(...maxLiked))
+  
+console.log('this is max',maxLiked)
+console.log('this is blogs.index',blogs[index])
+return blogs[index]
+  // const mostLiked = blogs.reduce((prev, curr) => {
+  //   return prev.likes > curr.likes ? prev : curr;
+  // });
+
+  // return {
+  //   title: mostLiked.title,
+  //   author: mostLiked.author,
+  //   likes: mostLiked.likes,
+  // };
+}
+
 module.exports = {
   dummy,
   totalLikes,
+  mostLikes,
 };
