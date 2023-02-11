4728275036c9277f1bd9f593a3b0533b4c090ab9
Justin Dickson <112723736+jay-nakagawa@users.noreply.github.com>
2023-02-11 18:09:34 -0500

    4.17 started

--- Part-4/blog/controllers/blogs.js
+++ Part-4/blog/controllers/blogs.js
@@ -1,5 +1,6 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
+const User = require('../models/user')
 
 blogsRouter.get("/", async (request, response) => {
   const blogs = await Blog.find({});
@@ -7,14 +8,31 @@ blogsRouter.get("/", async (request, response) => {
 });
 
 blogsRouter.post("/", async (request, response, next) => {
-  const blog = new Blog(request.body);
-  try {
-    const newBlog = await blog.save();
-    response.status(201).json(newBlog);
-  } catch (exception) {
-    response.status(400).end();
-    next(exception);
-  }
+  const body = request.body;
+  // console.log("body",request.body)
+  const user = await User.findById(body.id);
+  console.log("this is user", user)
+
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    user: user.id
+  });
+
+  const savedBlog = await blog.save()
+  user.blogs = user.notes.concat(savedBlog._id)
+  await user.save
+  response.json(savedBlog)
+
+  // const blog = new Blog(request.body);
+  // try {
+  //   const newBlog = await blog.save();
+  //   response.status(201).json(newBlog);
+  // } catch (exception) {
+  //   response.status(400).end();
+  //   next(exception);
+  // }
 });
 
 blogsRouter.delete("/:id", async (request, response, next) => {
@@ -27,8 +45,8 @@ blogsRouter.delete("/:id", async (request, response, next) => {
 });
 
 blogsRouter.put("/:id", async (request, response, next) => {
-  const blog = request.body
-  
+  const blog = request.body;
+
   Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
     .then((updatedBlog) => {
       response.json(updatedBlog);
