3051c0a682fd02b7b8a993172e800f1e45174172
Justin Dickson <112723736+jay-nakagawa@users.noreply.github.com>
2023-02-10 23:05:07 -0500

    4.15 complete -can get and post users

--- Part-4/blog/tests/blog_api.test.js
+++ Part-4/blog/tests/blog_api.test.js
@@ -1,10 +1,15 @@
 const mongoose = require("mongoose");
 const supertest = require("supertest");
+const bcrypt = require('bcrypt')
+
+const helper = require('./test_helper')
+
 const { response } = require("../app");
 const app = require("../app");
-const Blog = require("../models/blog");
-
 const api = supertest(app);
+const Blog = require("../models/blog");
+const User = require('../models/user')
+
 
 const initialBlogs = [
   {
@@ -142,6 +147,42 @@ test("a Blog can be updated", async () => {
 
   expect(updatedBlogs[0].likes).toEqual(111);
 });
+
+///////////user tests//////////
+
+describe('when there is initially one user in db', () => {
+  beforeEach(async () => {
+    await User.deleteMany({})
+
+    const passwordHash = await bcrypt.hash('sekret', 10)
+    const user = new User({ username: 'root', passwordHash })
+
+    await user.save()
+  },10000)
+
+  test('creation succeeds with a fresh username', async () => {
+    
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'mluukkai',
+      name: 'Matti Luukkainen',
+      password: 'salainen',
+    }
+
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const usersAtEnd = await helper.usersInDb()
+    expect(usersAtEnd).toHaveLength(usersAtStart.length + 1)
+
+    const usernames = usersAtEnd.map(u => u.username)
+    expect(usernames).toContain(newUser.username)
+  })
+})
 
 afterAll(async () => {
   await mongoose.connection.close();
