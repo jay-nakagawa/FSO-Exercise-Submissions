b21985d2e170b314f39b516964dbf1038200e993
jay-nakagawa <j.dickson2203@gmail.com>
2023-03-13 14:58:47 -0700

    -extracted login form -conditional rendering on login form

--- Part-5/bloglist-frontend/src/App.js
+++ Part-5/bloglist-frontend/src/App.js
@@ -1,9 +1,11 @@
 import { useState, useEffect } from "react";
 import Blog from "./components/Blog";
 import BlogForm from "./components/BlogForm";
+import LoginForm from "./components/LoginForm";
 import Notification from "./components/Notification";
 import blogService from "./services/blogs";
 import loginService from "./services/login";
+
 
 const App = () => {
   const [blogs, setBlogs] = useState([]);
@@ -11,6 +13,8 @@ const App = () => {
   const [password, setPassword] = useState("");
   const [user, setUser] = useState(null);
   const [message, setMessage] = useState(null);
+  const [loginVisible, setLoginVisible] = useState(false)
+
 
   const [newAuthor, setNewAuthor] = useState("");
   const [newTitle, setNewTitle] = useState("");
@@ -80,29 +84,53 @@ const App = () => {
     window.location.reload(false);
   };
 
-  const loginForm = () => (
-    <form onSubmit={handleLogin}>
+  // const loginForm = () => (
+  //   <form onSubmit={handleLogin}>
+  //     <div>
+  //       username
+  //       <input
+  //         type="text"
+  //         value={username}
+  //         name="Username"
+  //         onChange={({ target }) => setUsername(target.value)}
+  //       />
+  //     </div>
+  //     <div>
+  //       password
+  //       <input
+  //         type="password"
+  //         value={password}
+  //         name="Password"
+  //         onChange={({ target }) => setPassword(target.value)}
+  //       />
+  //     </div>
+  //     <button type="submit">login</button>
+  //   </form>
+  // );
+
+  const loginForm = () => {
+    const hideWhenVisible = { display: loginVisible ? 'none' : '' }
+    const showWhenVisible = { display: loginVisible ? '' : 'none' }
+
+    return (
       <div>
-        username
-        <input
-          type="text"
-          value={username}
-          name="Username"
-          onChange={({ target }) => setUsername(target.value)}
-        />
+        <div style={hideWhenVisible}>
+          <button onClick={() => setLoginVisible(true)}>log in</button>
+        </div>
+        <div style={showWhenVisible}>
+          <LoginForm
+            username={username}
+            password={password}
+            handleUsernameChange={({ target }) => setUsername(target.value)}
+            handlePasswordChange={({ target }) => setPassword(target.value)}
+            handleSubmit={handleLogin}
+          />
+          <button onClick={() => setLoginVisible(false)}>cancel</button>
+        </div>
       </div>
-      <div>
-        password
-        <input
-          type="password"
-          value={password}
-          name="Password"
-          onChange={({ target }) => setPassword(target.value)}
-        />
-      </div>
-      <button type="submit">login</button>
-    </form>
-  );
+    )
+  }
+
 
   // const blogForm = () => (
   //   <form onSubmit={addBlog}>
@@ -135,8 +163,9 @@ const App = () => {
   //   </form>
   // );
 
- 
 
+
+ 
   const blogList = () =>
     blogs.map((blog) => <Blog key={blog.id} blog={blog} />);
 
