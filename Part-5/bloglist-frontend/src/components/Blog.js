e0530c70bbc9b1c04f81b33c14866af43d42efc7
jay-nakagawa <j.dickson2203@gmail.com>
2023-03-19 11:09:55 -0700

    5.7 started -shows additional blog information

--- Part-5/bloglist-frontend/src/components/Blog.js
+++ Part-5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,12 @@
-const Blog = ({blog}) => (
+
+
+const Blog = ({blog}) => {
+  
+  
+  return (
   <div>
-    {blog.title} {blog.author}
+    {blog.title} {blog.author} {blog.likes} {blog.url}
   </div>  
-)
+)}
 
 export default Blog
