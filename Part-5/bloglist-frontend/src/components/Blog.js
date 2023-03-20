3a52c7929b359b3894d7658ba598d31bf138ea28
jay-nakagawa <j.dickson2203@gmail.com>
2023-03-20 11:44:03 -0700

    5.7 complete -added button show additional blog info -added styles

--- Part-5/bloglist-frontend/src/components/Blog.js
+++ Part-5/bloglist-frontend/src/components/Blog.js
@@ -1,11 +1,31 @@
-
+import { useState } from "react"
 
 const Blog = ({blog}) => {
+  const [visible, setVisible] = useState(false)
   
+  const toggleVisible = () => {
+    setVisible(!visible)
+  }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
   
   return (
-  <div>
-    {blog.title} {blog.author} {blog.likes} {blog.url}
+  <div style={blogStyle}>
+    <div>
+      {blog.title}
+      <button onClick={toggleVisible}>{visible? "hide" : "show"}</button>
+    </div>
+    {visible && 
+    <div>
+      {blog.author} {blog.likes} {blog.url}
+    </div>}
+     
   </div>  
 )}
 
