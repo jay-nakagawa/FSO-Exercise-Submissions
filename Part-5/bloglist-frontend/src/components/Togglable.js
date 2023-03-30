05cd619e9a681f422a5fe6b58b764e6ca52f99fe
jay-nakagawa <j.dickson2203@gmail.com>
2023-03-29 20:50:32 -0700

    5.12 complete? -added eslint config -fixed errors

--- Part-5/bloglist-frontend/src/components/Togglable.js
+++ Part-5/bloglist-frontend/src/components/Togglable.js
@@ -1,25 +1,25 @@
-import { useState, forwardRef, useImperativeHandle } from 'react'
+import { useState, forwardRef, useImperativeHandle } from "react";
 import PropTypes from "prop-types";
 
-const Togglable = forwardRef((props,refs) => {
-  const [visible, setVisible] = useState(false)
+const Togglable = forwardRef((props, refs) => {
+  const [visible, setVisible] = useState(false);
 
-  const hideWhenVisible = { display: visible ? 'none' : '' }
-  const showWhenVisible = { display: visible ? '' : 'none' }
+  const hideWhenVisible = { display: visible ? "none" : "" };
+  const showWhenVisible = { display: visible ? "" : "none" };
 
   const toggleVisibility = () => {
-    setVisible(!visible)
-  }
-  
-  Togglable.propTypes = {
-    buttonLabel: PropTypes.string.isRequired
-  }
+    setVisible(!visible);
+  };
 
-  useImperativeHandle(refs,()=>{
-    return{
-      toggleVisibility
-    }
-  })
+  Togglable.propTypes = {
+    buttonLabel: PropTypes.string.isRequired,
+  };
+
+  useImperativeHandle(refs, () => {
+    return {
+      toggleVisibility,
+    };
+  });
 
   return (
     <div>
@@ -31,7 +31,9 @@ const Togglable = forwardRef((props,refs) => {
         <button onClick={toggleVisibility}>cancel</button>
       </div>
     </div>
-  )
-})
+  );
+});
 
-export default Togglable
+Togglable.displayName = "Togglable";
+
+export default Togglable;
