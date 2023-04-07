6aa59b9b8f177148f045ed70d9471aa07e0fa04e
jay-nakagawa <j.dickson2203@gmail.com>
2023-04-06 18:31:13 -0700

    5.17 complete -configured for cypress

--- Part-5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ Part-5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -7,6 +7,12 @@
 describe("Blog app", function () {
   beforeEach(function () {
     cy.request("POST", "http://localhost:3003/api/testing/reset");
+    const user = {
+      name: "jay",
+      username: "admin",
+      password: "pass123",
+    };
+    cy.request("POST", "http://localhost:3003/api/users/", user);
     cy.visit("http://localhost:3000");
   });
   it("front page can be opened", function () {
