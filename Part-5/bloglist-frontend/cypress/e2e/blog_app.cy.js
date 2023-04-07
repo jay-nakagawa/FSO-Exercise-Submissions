886eeabdb2beb0714967273a5d98ae12abb60bce
jay-nakagawa <j.dickson2203@gmail.com>
2023-04-06 17:58:41 -0700

    config testing db for backend

--- Part-5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ Part-5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -5,6 +5,10 @@
 // });..
 
 describe("Blog app", function () {
+  beforeEach(function () {
+    cy.request("POST", "http://localhost:3003/api/testing/reset");
+    cy.visit("http://localhost:3000");
+  });
   it("front page can be opened", function () {
     cy.visit("http://localhost:3000");
     cy.contains("blogs");
