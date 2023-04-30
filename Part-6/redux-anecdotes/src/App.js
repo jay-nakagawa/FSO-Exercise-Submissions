ef7a9eed37342450dc54d7e699f9abaab2165d4a
Justin Dickson <112723736+jay-nakagawa@users.noreply.github.com>
2023-04-30 16:53:14 -0400

    6.9 started -added filter component

--- Part-6/redux-anecdotes/src/App.js
+++ Part-6/redux-anecdotes/src/App.js
@@ -1,10 +1,11 @@
 import AnecdoteForm from "./components/anecdoteForm";
 import AnecdoteList from "./components/AnecdoteList";
+import Filter from "./components/Filter";
 
 const App = () => {
   return (
     <div>
-      
+      <Filter />
       <AnecdoteList />
       <AnecdoteForm />
     </div>
---
+++ Part-6/redux-anecdotes/src/components/Filter.js
@@ -0,0 +1,17 @@
+const Filter = () => {
+    const handleChange = (event) => {
+        console.log(event.target.value)
+      // input-field value is in variable event.target.value
+    }
+    const style = {
+      marginBottom: 10
+    }
+  
+    return (
+      <div style={style}>
+        filter <input onChange={handleChange} />
+      </div>
+    )
+  }
+  
+  export default Filter
