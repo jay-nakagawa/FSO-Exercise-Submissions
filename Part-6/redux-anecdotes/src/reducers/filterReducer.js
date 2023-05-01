541e75bf1d7e92990b323544c20cd7078648a06b
Justin Dickson <112723736+jay-nakagawa@users.noreply.github.com>
2023-05-01 16:19:12 -0400

    6.10 complete -filter reducer and action creator now use createSlice

--- Part-6/redux-anecdotes/src/reducers/filterReducer.js
+++ Part-6/redux-anecdotes/src/reducers/filterReducer.js
@@ -1,17 +1,31 @@
-const filterReducer = (state = "", action) => {
-  switch (action.type) {
-    case "SET_FILTER":
+import { createSlice } from "@reduxjs/toolkit";
+
+const filterSlice = createSlice({
+  name: "filter",
+  initialState: "",
+  reducers: {
+    filterChange(state, action) {
       return action.payload;
-    default:
-      return state;
-  }
-};
+    },
+  },
+});
 
-export const filterChange = (filterValue) => {
-  return {
-    type: "SET_FILTER",
-    payload: filterValue,
-  };
-};
+// const filterReducer = (state = "", action) => {
+//   switch (action.type) {
+//     case "SET_FILTER":
+//       return action.payload;
+//     default:
+//       return state;
+//   }
+// };
 
-export default filterReducer;
+// export const filterChange = (filterValue) => {
+//   return {
+//     type: "SET_FILTER",
+//     payload: filterValue,
+//   };
+// };
+
+// export default filterReducer;
+export const { filterChange } = filterSlice.actions;
+export default filterSlice.reducer;
