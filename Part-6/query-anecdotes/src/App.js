1d807b77ec121bd332890a803ac6e2f8a3b5114a
Justin Dickson <112723736+jay-nakagawa@users.noreply.github.com>
2023-05-08 16:47:51 -0400

    6.20 started

---
+++ Part-6/query-anecdotes/src/App.js
@@ -0,0 +1,44 @@
+import { useQuery } from "react-query";
+import { getAnecdotes } from "./requests";
+
+
+
+import AnecdoteForm from "./components/AnecdoteForm";
+import Notification from "./components/Notification";
+
+const App = () => {
+  
+  const result = useQuery("anecdotes", getAnecdotes)  
+  console.log(result);
+
+  if (result.isLoading) {
+    return <div>loading data...</div>;
+  }
+
+  const anecdotes = result.data;
+
+  const handleVote = (anecdote) => {
+    console.log("vote");
+  };
+
+  return (
+    <div>
+      <h3>Anecdote app</h3>
+
+      <Notification />
+      <AnecdoteForm />
+
+      {anecdotes.map((anecdote) => (
+        <div key={anecdote.id}>
+          <div>{anecdote.content}</div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => handleVote(anecdote)}>vote</button>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default App;
