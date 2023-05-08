f9fbe92fc5adbb16ee2e0e2886201514486a77b9
Justin Dickson <112723736+jay-nakagawa@users.noreply.github.com>
2023-05-08 16:56:36 -0400

    6.20 complete - retrieval of data using react query -added error handling

--- Part-6/query-anecdotes/src/App.js
+++ Part-6/query-anecdotes/src/App.js
@@ -7,13 +7,24 @@ import AnecdoteForm from "./components/AnecdoteForm";
 import Notification from "./components/Notification";
 
 const App = () => {
-  
-  const result = useQuery("anecdotes", getAnecdotes)  
+
+  const result = useQuery(
+    'anecdotes', getAnecdotes, 
+    {
+      retry: false
+    }
+  )
+
   console.log(result);
 
   if (result.isLoading) {
     return <div>loading data...</div>;
   }
+
+  if (result.isError) {
+    return <span>Error: {result.error.message}</span>
+  }
+
 
   const anecdotes = result.data;
 
