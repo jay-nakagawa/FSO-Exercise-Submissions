70d2b7f2a59eb15cf050418d761a4f5ea2162776
Justin Dickson <112723736+jay-nakagawa@users.noreply.github.com>
2023-05-10 19:18:57 -0400

    6.23 started - notification rendered upon vote

--- Part-6/query-anecdotes/src/App.js
+++ Part-6/query-anecdotes/src/App.js
@@ -1,32 +1,44 @@
 import { useQuery, useMutation, useQueryClient } from "react-query";
 import { getAnecdotes, updateAnecdote } from "./requests";
+import { useReducer } from "react";
 
 import AnecdoteForm from "./components/AnecdoteForm";
 import Notification from "./components/Notification";
 
+const notificationReducer = (state, action) => {
+  switch (action.type) {
+    case "VOTE":
+      return "you voted";
+    case "CREATE":
+      return { ...state, message: `you created '${action.data.content}'` };
+    default:
+      return state;
+  }
+};
+
 const App = () => {
+  const [notification, notificationDispatch] = useReducer(
+    notificationReducer,
+    null
+  );
   const queryClient = useQueryClient();
-  
+
   const result = useQuery("anecdotes", getAnecdotes, {
     retry: false,
   });
   const anecdotes = result.data;
   console.log(result);
 
-  const updateAnecdoteMutation = useMutation(updateAnecdote,{
+  const updateAnecdoteMutation = useMutation(updateAnecdote, {
     onSuccess: () => {
       queryClient.invalidateQueries("anecdotes");
-    }
+    },
   });
   const handleVote = (anecdote) => {
-    console.log(anecdote)
+    notificationDispatch({ type: "VOTE" });
+    console.log(anecdote);
     updateAnecdoteMutation.mutate({ ...anecdote, votes: anecdote.votes + 1 });
   };
-
-
- 
-
-  
 
   if (result.isLoading) {
     return <div>loading data...</div>;
@@ -36,17 +48,11 @@ const App = () => {
     return <span>Error: {result.error.message}</span>;
   }
 
- 
-
-
-
- 
-
   return (
     <div>
       <h3>Anecdote app</h3>
 
-      <Notification />
+      <Notification notification={notification} />
       <AnecdoteForm />
 
       {anecdotes.map((anecdote) => (
